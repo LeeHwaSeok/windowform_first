b969c428b3c0d6130bb373dc4b6e4099bdcb29ab
20171062@student.anu.ac.kr <20171062@student.anu.ac.kr>
2022-05-12 15:11:43 +0900

    1차 백업

--- HelloWorld/Form1.Designer.cs
+++ HelloWorld/Form1.Designer.cs
@@ -44,6 +44,17 @@
             this.radioButton1 = new System.Windows.Forms.RadioButton();
             this.radioButton2 = new System.Windows.Forms.RadioButton();
             this.button1 = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label9 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -160,7 +171,7 @@
             // 
             this.label7.AutoSize = true;
             this.label7.Font = new System.Drawing.Font("굴림", 18F);
-            this.label7.Location = new System.Drawing.Point(65, 398);
+            this.label7.Location = new System.Drawing.Point(337, 269);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(0, 36);
             this.label7.TabIndex = 11;
@@ -171,9 +182,9 @@
             this.groupBox1.Controls.Add(this.button1);
             this.groupBox1.Controls.Add(this.radioButton2);
             this.groupBox1.Controls.Add(this.radioButton1);
-            this.groupBox1.Location = new System.Drawing.Point(85, 502);
+            this.groupBox1.Location = new System.Drawing.Point(84, 395);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(623, 195);
+            this.groupBox1.Size = new System.Drawing.Size(554, 145);
             this.groupBox1.TabIndex = 12;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "개인정보 동의";
@@ -211,11 +222,127 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click_1);
             // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(81, 555);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(146, 18);
+            this.label8.TabIndex = 13;
+            this.label8.Text = "주문 시 요구사항";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(84, 590);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(550, 28);
+            this.textBox1.TabIndex = 14;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(690, 583);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(170, 38);
+            this.button2.TabIndex = 14;
+            this.button2.Text = "접수";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(81, 639);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(0, 18);
+            this.label9.TabIndex = 15;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "신용카드",
+            "무통장입금",
+            "포인트사용"});
+            this.comboBox1.Location = new System.Drawing.Point(84, 778);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 26);
+            this.comboBox1.TabIndex = 16;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 18;
+            this.listBox1.Location = new System.Drawing.Point(229, 778);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(435, 94);
+            this.listBox1.TabIndex = 17;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(81, 729);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(62, 18);
+            this.label10.TabIndex = 18;
+            this.label10.Text = "결제란";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(81, 884);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(80, 18);
+            this.label11.TabIndex = 19;
+            this.label11.Text = "주문결과";
+            this.label11.Click += new System.EventHandler(this.label11_Click);
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(81, 757);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(86, 18);
+            this.label12.TabIndex = 20;
+            this.label12.Text = "결제 수단";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(226, 757);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(122, 18);
+            this.label13.TabIndex = 21;
+            this.label13.Text = "결제 요청사항";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(690, 778);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(170, 38);
+            this.button3.TabIndex = 22;
+            this.button3.Text = "결제";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1132, 808);
+            this.ClientSize = new System.Drawing.Size(1094, 1012);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label8);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.label7);
             this.Controls.Add(this.label6);
@@ -257,6 +384,17 @@
         private System.Windows.Forms.RadioButton radioButton2;
         private System.Windows.Forms.RadioButton radioButton1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Button button3;
     }
 }
 
--- HelloWorld/Form1.cs
+++ HelloWorld/Form1.cs
@@ -19,17 +19,10 @@ namespace HelloWorld
 
         private void button1_Click(object sender, EventArgs e)
         {
-
-            label3.Text = "";
-            label4.Text = "";
-            label5.Text = "";
-            label6.Text = "";
             label7.Text = "후기를 남겨 주시면 이벤트에 응모됩니다. \n이벤트에 응모하려면 개인정보 동의해 주세요.";
 
-
-
-
-            if (checkBox1.Checked) {
+            if (checkBox1.Checked)
+            {
                 label3.Text = checkBox1.Text;
             }
             if (checkBox2.Checked)
@@ -46,9 +39,53 @@ namespace HelloWorld
             }
 
         }
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked)
+            {
+                MessageBox.Show("응모완료", "응모 확인");
+            }
+            else if (radioButton2.Checked)
+            {
+                MessageBox.Show("응모","응모 확인");
+            }
+            else
+            {
+                MessageBox.Show("개인정보 동의 여부 체크하세요", "응모 확인");
+            }
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string a = "", b = "", c = "", d = "", f = "";
+            if (checkBox1.Checked)
+            {
+                a = checkBox1.Text + " ";
+            }
+            else if (checkBox2.Checked)
+            {
+                b = checkBox2.Text + " ";
+            }
+            else if (checkBox3.Checked)
+            {
+                c = checkBox3.Text + " ";
+            }
+            else if (checkBox4.Checked)
+            {
+                d = checkBox4.Text + " ";
+            }
+            else { a = "주문하지 않음";  }
+            f = textBox1.Text + " ";
 
+            label9.Text = "주문하신 메뉴는 '" + a + b + c + d + "'입니다." + "\n주문 시 요구사항은 " + f + "입니다.";
+
+
+        }
         private void Form1_Load(object sender, EventArgs e)
         {
+            label3.Text = "";
+            label4.Text = "";
+            label5.Text = "";
+            label6.Text = "";
 
         }
 
@@ -88,20 +125,40 @@ namespace HelloWorld
 
         }
 
-        private void button1_Click_1(object sender, EventArgs e)
+        private void textBox1_TextChanged(object sender, EventArgs e)
         {
-            if (radioButton1.Checked)
-            {
-                MessageBox.Show("응모완료");
-            }
-            else if (radioButton2.Checked)
-            {
-                MessageBox.Show("응모");
-            }
-            else
-            {
-                MessageBox.Show("개인정보 동의 여부 체크하세요");
-            }
+
+        }
+
+        private void label11_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+
+            string[] lst1 = { "나라사랑카드", "다라사랑카드", "라라사랑카드" };
+            string[] lst2 = { "무통장입금", "유통장입금", "부통장입금" };
+            string[] lst3 = { "포인트사용", "포인트4용", "포인드" };
+
+            if (comboBox1.SelectedIndex == 0) listBox1.Items.AddRange(lst1);
+            else if (comboBox1.SelectedIndex == 1) listBox1.Items.AddRange(lst2);
+            else if (comboBox1.SelectedIndex == 2) listBox1.Items.AddRange(lst3);
+
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+            MessageBox.Show("결제 수단 : " + comboBox1.Text +" " + "\n할부 종류 : "+listBox1.Text, "결제 확인");
         }
     }
 }
